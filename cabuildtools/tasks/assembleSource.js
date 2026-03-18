71fd00aa2ea0b3264c70df41df402a15c667d5bf
南鸢晨星 <YS_hutao520@163.com>
2026-03-19 07:33:54 +0800

    fix: 修正编译任务的路径
    - 从 ../ca/main.js 加载
    
    - 确保在所有工作目录下都能正确找到入口文件

--- cabuildtools/tasks/assembleSource.js
+++ cabuildtools/tasks/assembleSource.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const loader = require("../loader-ast");
 module.exports = function(context, args) {
-	var result = loader.load("./main.js", { buildConfig : context.buildConfig });
+	var result = loader.load("../ca/main.js", { buildConfig : context.buildConfig });
 	return result;
 };
