92fad36fa655d53279a87f8a577c29def12bf2ac
南鸢晨星 <YS_hutao520@163.com>
2026-03-19 07:33:07 +0800

    build: 所有编译流程切换到 AST Loader
    - assembleSourceRelease.js
    
    - assembleSourceSnapshot.js
    
    - assembleSource.js (之前已更新)

--- cabuildtools/tasks/assembleSourceRelease.js
+++ cabuildtools/tasks/assembleSourceRelease.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const loader = require("../loader");
+const loader = require("../loader-ast");
 module.exports = function(context, args) {
 	var result = loader.load("./main.js", { buildConfig : context.buildConfig });
 	fs.writeFileSync("./build/outputs/releaseSource/main.js", result);
