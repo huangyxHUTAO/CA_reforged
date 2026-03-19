660df6b343e2741492f518492f88da31acfa50e3
南鸢晨星 <YS_hutao520@163.com>
2026-03-19 08:41:21 +0800

    fix: FileLogger 使用应用私有目录
    - 适配 Android 11+ 存储权限限制
    
    - 避免写入外部存储根目录失败

--- ca/modules/test/FileLogger.js
+++ ca/modules/test/FileLogger.js
@@ -1,5 +1,6 @@
 /*LOADER TestOnly()*/
-var fp = new android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/ca.debug.txt";
+// 使用应用私有目录，避免 Android 11+ 存储权限限制
+var fp = ctx.getExternalFilesDir(null).getAbsolutePath() + "/ca.debug.txt";
 var pw = new java.io.PrintWriter(fp);
 Log.start(function(tag, data) {
 	pw.println(new Date() + " " + tag + "\n" + data);
