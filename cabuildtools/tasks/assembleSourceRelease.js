fe1a7909fec2fa979aff916755877dd48c2e0fdf
南鸢晨星 <YS_hutao520@163.com>
2026-03-25 13:26:21 +0800

    feat: 在构建任务中启用 async/await 转译
    - assembleSource.js
    - assembleSourceRelease.js
    - assembleSourceSnapshot.js
    
    全部启用 transformAsync: true

--- cabuildtools/tasks/assembleSourceRelease.js
+++ cabuildtools/tasks/assembleSourceRelease.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const loader = require("../loader-ast");
 module.exports = function(context, args) {
-	var result = loader.load("../ca/main.js", { buildConfig : context.buildConfig });
+	var result = loader.load("../ca/main.js", { buildConfig : context.buildConfig }, "utf-8", { transformAsync: true });
 	fs.writeFileSync("./build/outputs/releaseSource/main.js", result);
 	return result;
 }
