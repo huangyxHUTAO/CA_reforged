5fdc0d6ffb466908037b6977fd67818b23a3ed29
南鸢晨星 <YS_hutao520@163.com>
2026-05-23 16:15:19 +0800

    refactor: 重构打包的拼接js行为，不使用正则替换而是AST

--- cabuildtools/tasks/assembleSourceRelease.js
+++ cabuildtools/tasks/assembleSourceRelease.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const loader = require("../loader-ast");
 module.exports = function(context, args) {
+	console.log("[assembleSourceRelease] 开始编译，transformAsync: true");
 	var result = loader.load("../ca/main.js", { buildConfig : context.buildConfig }, "utf-8", { transformAsync: true });
+	console.log("[assembleSourceRelease] 编译完成，包含 _asyncToGenerator: " + result.includes("_asyncToGenerator"));
 	fs.writeFileSync("./build/outputs/releaseSource/main.js", result);
 	return result;
-}
+};
--- cabuildtools/tasks/assembleSourceSnapshot.js
+++ cabuildtools/tasks/assembleSourceSnapshot.js
@@ -4,4 +4,4 @@ module.exports = function(context, args) {
 	var result = loader.load("../ca/main.js", { buildConfig : context.buildConfig }, "utf-8", { transformAsync: true });
 	fs.writeFileSync("./build/outputs/snapshotSource/main.js", result);
 	return result;
-}
+};
